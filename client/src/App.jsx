fbac86e5ec5c33fcb09f42163c68c65b3a9e4dcc
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2025-06-20 01:00:45 +0530

    chore: update dependencies and improve App component
    - Updated dependencies in package.json for better performance and compatibility.
    - Removed unused Tailwind CSS plugins and updated versions of React and Axios.
    - Modified App.css to fix formatting issues.
    - Enhanced App.jsx by adding state management and updating the welcome message.
    - Adjusted Vite configuration for consistency.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='text-4xl font-bold flex items-center justify-center'>Welcome to CityMall</h1>
+      <h1 className='text-4xl flex items-center justify-center h-screen'>
+        TEst
+      </h1>
     </>
   )
 }
