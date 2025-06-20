37fa0a975b1802b5c33718dcb8ac3ce722207cc4
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2025-06-20 11:04:24 +0530

    fix: update API URL to use the production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://disaster-res-server.onrender.com/api';
 
 // Create axios instance with base URL
 const api = axios.create({
